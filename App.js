ff693704d4e9f161d3a135a87c2118f1b74e9983
ihju <t0ihju00@students.oamk.fi>
2022-09-20 19:07:35 +0300

    alkometrin tekniikka toimii. Viimeistelyä vaille valmis

--- App.js
+++ App.js
@@ -1,11 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import RadioForm from 'react-native-simple-radio-button'
+import {useState} from 'react';
 
 export default function App() {
+  const [weight, setWeight] = useState(0)
+  const [bottles, setBottles] = useState(0)
+  const [time, setTime] = useState(0)
+  const [gender, setGender] = useState('male')
+  const [alc, setAlc] = useState(0)
+
+  const bottleCount = [
+    {label: '1', value: '1.0'},
+    {label: '2', value: '2.0'},
+    {label: '3', value: '3.0'},
+    {label: '4', value: '4.0'},
+    {label: '5', value: '5.0'},
+    {label: '6', value: '6.0'},
+    {label: '7', value: '7.0'},
+    {label: '8', value: '8.0'},
+    {label: '9', value: '9.0'},
+    {label: '10', value: '10.0'},
+    {label: '11', value: '11.0'},
+    {label: '12', value: '12.0'},
+    {label: '13', value: '13.0'},
+  ]
+
+  const genders = [
+    {label: 'Male', value: 'male'},
+    {label: 'Female', value: 'female'},
+  ]
+
+  const times = [
+    {label: '1', value: '1.0'},
+    {label: '2', value: '2.0'},
+    {label: '3', value: '3.0'},
+    {label: '4', value: '4.0'},
+    {label: '5', value: '5.0'},
+    {label: '6', value: '6.0'},
+    {label: '7', value: '7.0'},
+    {label: '8', value: '8.0'},
+    {label: '9', value: '9.0'},
+    {label: '10', value: '10.0'},
+    {label: '11', value: '11.0'},
+    {label: '12', value: '12.0'},
+    {label: '13', value: '13.0'},
+
+  ]
+
+  const calculate = () => {
+    let result = 0
+    let litres = 0
+    let grams = 0
+    let burning = 0
+    let gramsLeft = 0
+
+    litres = bottles*0.33
+    grams = litres*8*4.5
+    burning = weight/10
+    gramsLeft = grams - (burning*time)
+
+    if(weight != 0){
+      if(gender === 'male'){
+        result = gramsLeft/(weight*0.7)
+      }else{
+        result = gramsLeft/(weight*0.6)
+      }
+      if(result < 0){
+        setAlc(0)
+      }else{setAlc(result)}
+    } else{alert("Please insert your weight")}
+    
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Alcometer</Text>
+      <Text>Weight</Text>
+      <TextInput placeholder="Add weight..."  value={weight} onChangeText={text => setWeight(text)}  />
+      <Text>Bottles</Text>
+      <Picker onValueChange={(itemValue) => setBottles(itemValue)} selectedValue={bottles}>
+        {
+          bottleCount.map((bottles,index) => (
+            <Picker.Item key={index} label={bottles.label} value={bottles.value} />
+          ))
+        }
+
+      </Picker>
+      <Text>Time</Text>
+      <Picker onValueChange={(itemValue) => setTime(itemValue)} selectedValue={time}>
+      {
+          times.map((time,index) => (
+            <Picker.Item key={index} label={time.label} value={time.value} />
+          ))
+        }
+      </Picker>
+      <Text>Gender</Text>
+      <RadioForm
+      buttonSize={10}
+      radio_props={genders}
+      initial={0}
+      onPress={(value) => setGender(value)}
+      />
+      <Text>{alc.toFixed(2)}</Text>
+      <Button title="Calculate" onPress={calculate}/>
     </View>
   );
 }
@@ -13,8 +113,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
